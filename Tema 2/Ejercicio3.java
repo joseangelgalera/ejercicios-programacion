d8c095be5d98fcdd29e27bc4557abe051e38684d
José Ángel Galera García <joseangelgg96@gmail.com>
2017-10-04 09:36:12 +0200

    Actualizando README.md

---
+++ Tema 2/Ejercicio3.java
@@ -0,0 +1,25 @@
+/*
+ * José Ángel Galera García
+ * 
+ * Ejercicio3.java
+ * 
+ * Tema 2
+ * 
+ * Crea las variables nombre, direccion y telefono y asígnale los valores 
+ * correspondientes. Muestra los valores de esas variables por pantalla de tal 
+ * forma que el resultado del programa sea el mismo que en el ejercicio 2.
+ */
+
+
+public class Ejercicio3 {
+	public static void main (String args[]) {
+		String nombre = "José Ángel";
+    String direccion = "Calle Moral";
+    String telefono = "687 349 704";
+    
+    System.out.println(nombre);
+    System.out.println(direccion);
+    System.out.println(telefono);
+	}
+}
+
