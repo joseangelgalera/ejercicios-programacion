faed3ec58fa004b8d4c982ea45793faec89fc710
José Ángel Galera García <joseangelgg96@gmail.com>
2017-10-18 10:20:51 +0200

    Añadido ejercicio 13 tema 5

---
+++ Tema 5/Ejercicio13.java
@@ -0,0 +1,32 @@
+/*
+ * Tema 5
+ * 
+ * Ejercicio 13
+ * 
+ * Escribe un programa que lea una lista de diez números y determine cuántos
+ * son positivos, y cuántos son negativos.
+ * 
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio13 {
+  public static void main (String args[]) {
+    Scanner s = new Scanner(System.in);
+    
+    System.out.println ("Introduzca diez números enteros : ");
+    int n = 0;
+    int p = 0;
+    
+    for (int i = 0; i < 10; i++) {
+      if (s.nextInt() < 0) {
+        n++;
+      } else {
+        p++;
+      }
+    }
+    
+    System.out.print("Ha introducido " + n + " numeros negativos y " + 
+      p + " numeros positivos");
+  }
+}
