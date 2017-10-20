8dc2e4bc8bbc71755a0396c7b56423a20886defc
José Ángel Galera García <joseangelgg96@gmail.com>
2017-10-20 16:48:42 +0200

    Añadidos ejercicios 6 y 7 del tema 5

--- Tema 5/Ejercicio5.java
+++ Tema 5/Ejercicio5.java
@@ -1,19 +1,22 @@
 /*
  * Tema 5
  * 
- * Ejercicio 4
+ * Ejercicio 5
  * 
  * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
- * utilizando un bucle for .
+ * utilizando un bucle while .
  * 
  */
 
 
-public class Ejercicio4 {
+public class Ejercicio5 {
   public static void main (String args[]) {
     
-    for (int i = 320 ; i >= 160 ; i -= 20) {
+    int i = 320;
+    
+    while (i >= 160) {
       System.out.println(i);
+      i -= 20;
     }
   }
 }
