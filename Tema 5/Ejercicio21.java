8c597acd7f379935a941e6d7335d4d8d7a3f68b6
joseangelgalera <joseangelgg96@gmail.com>
2017-10-25 09:34:34 +0200

    Terminado ejercicio 21 tema 5

--- Tema 5/Ejercicio21.java
+++ Tema 5/Ejercicio21.java
@@ -21,16 +21,32 @@ public class Ejercicio21 {
     System.out.println("introduzca uno negativo).");
     
     double contador = 0;
-    double numero= 0;
+    double numero = 0;
+    double maximoPar = 0;
+    double sumaImpares = 0;
+    double numerosImpares = 0;
+    
     
     while (numero >= 0) {
       numero = s.nextInt();
       if (numero >= 0) {
         contador++;
       }
-      if (numero % 0)
+      if (((numero % 2) == 0) && (numero > maximoPar)) {
+        maximoPar = numero;
+      } else { 
+        
+        if ((numero % 2) == 1 ) {
+          sumaImpares += numero;
+          numerosImpares++;
+        }
+      } 
     }
-    System.out.print(contador +"");
+     
+     System.out.println("Ha introducido " + contador + " numeros");
+     System.out.println("La media de los numeros impares es " 
+     + sumaImpares / numerosImpares);
+     System.out.print("El maximo par es " + maximoPar);
   }
 }
 
