6d52cef6f4e523f88f900d30924901c6cb480ee7
José Ángel Galera García <joseangelgg96@hotmail.com>
2018-04-24 10:13:29 +0200

    Añadido ejercicio 2

---
+++ Tema 1/Ejercicio2.java
@@ -0,0 +1,16 @@
+/*
+ * Modifica el programa anterior para que además se muestre tu dirección y
+ * tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+ * separadas.
+ */
+package tema.pkg1;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        System.out.println("José Ángel");
+        System.out.println("C/Moral nº 14");
+        System.out.println("687349704");
+    }
+    
+}
