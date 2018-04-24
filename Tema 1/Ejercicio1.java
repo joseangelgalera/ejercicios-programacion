6baf43285c9f73aba37a30a483e8bfad5605bcd0
joseangelgalera <joseangelgg96@gmail>
2018-04-24 10:05:12 +0200

    Actualizado

---
+++ Tema 1/Ejercicio1.java
@@ -0,0 +1,13 @@
+/*
+ * Escribe una programa que muestre tu nombre por pantalla.
+ *
+ */
+package tema.pkg1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.print("José Ángel");
+    }
+    
+}
