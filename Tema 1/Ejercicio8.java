463d1b7fb21627f9281a64be3617a566a626410d
joseangelgalera <joseangelgalera@github.com>
2018-04-24 12:24:57 +0200

    Añadido ejercicio 8

---
+++ Tema 1/Ejercicio8.java
@@ -0,0 +1,19 @@
+/*
+ * Ejercicio 8
+ * 
+ * Igual que el programa anterior, pero esta vez la pirámide debe aparecer
+ * invertida, con el vértice hacia abajo.
+ */
+package tema.pkg1;
+
+public class Ejercicio8 {
+
+    public static void main(String[] args) {
+        System.out.printf("%9s\n", "*********");
+        System.out.printf("%8s\n", "*     *");
+        System.out.printf("%7s\n", "*   *");
+        System.out.printf("%6s\n", "* *");
+        System.out.printf("%5s\n", "*");
+    }
+    
+}
