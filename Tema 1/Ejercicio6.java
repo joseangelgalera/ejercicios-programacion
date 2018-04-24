ba8e03c919da880e47add8dd670c77f03640dd8f
joseangelgalera <joseangelgalera@github.com>
2018-04-24 12:17:44 +0200

    Añadido ejercicio 6

---
+++ Tema 1/Ejercicio6.java
@@ -0,0 +1,19 @@
+/*
+ * Ejercicio 6
+ * 
+ * Escribe un programa que pinte por pantalla una pirámide rellena a base de
+ * asteriscos. La base de la pirámide debe estar formada por 9 asteriscos.
+ */
+package tema.pkg1;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+        System.out.printf("%5s\n", "*");
+        System.out.printf("%6s\n", "***");
+        System.out.printf("%7s\n", "*****");
+        System.out.printf("%8s\n", "*******");
+        System.out.printf("%9s\n", "*********");
+    }
+    
+}
