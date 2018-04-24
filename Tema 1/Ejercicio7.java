734cc098f19273c321af2c6585361dfdfc63dfa8
joseangelgalera <joseangelgalera@github.com>
2018-04-24 12:21:06 +0200

    Añadido ejercicio 6

---
+++ Tema 1/Ejercicio7.java
@@ -0,0 +1,19 @@
+/*
+ * Ejercicio 7
+ *
+ * Igual que el programa anterior, pero esta vez la pirámide estará hueca (se
+ * debe ver únicamente el contorno hecho con asteriscos).
+ */
+package tema.pkg1;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        System.out.printf("%5s\n", "*");
+        System.out.printf("%6s\n", "* *");
+        System.out.printf("%7s\n", "*   *");
+        System.out.printf("%8s\n", "*     *");
+        System.out.printf("%9s\n", "*********");
+    }
+    
+}
