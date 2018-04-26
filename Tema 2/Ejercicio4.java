d480404fd9f2508a78c9e0a232477b07a42e9eeb
joseangelgalera <joseangelgalera@github.com>
2018-04-26 09:11:32 +0200

    Añadido ejercicio 4

---
+++ Tema 2/Ejercicio4.java
@@ -0,0 +1,19 @@
+/*
+ * Tema 2
+ *
+ * Ejercicio 4
+ *
+ * Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere
+ * convertir deberá estar almacenada en una variable.
+ */
+package tema.pkg2;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+       int euro = 5 ;
+       
+       System.out.print(euro + " euros son " + (euro * 166) + " pesetas.");
+    }
+    
+}
