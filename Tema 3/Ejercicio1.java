c306e5cf35c9c46f4757a50e3321307c26893e70
joseangelgalera <joseangelgalera@github.com>
2018-04-26 09:37:32 +0200

    Añadido ejercicio 1

---
+++ Tema 3/Ejercicio1.java
@@ -0,0 +1,26 @@
+/*
+ * Tema 3
+ * 
+ * Ejercicio 1
+ *
+ * Realiza un programa que pida dos números y que luego muestre el resultado
+ * de su multiplicación.
+ */
+package tema.pkg3;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Introduce la x: ");
+        int x = s.nextInt();
+        System.out.print("Introduce la y: ");
+        int y = s.nextInt();
+        
+        System.out.print(x + (" * ") + y + (" = ") + (x * y));
+    }
+    
+}
