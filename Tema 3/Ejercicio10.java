206b7b25fc6d7b1626f785f345d132ff60e2c599
joseangelgalera <joseangelgalera@github.com>
2018-04-26 11:16:09 +0200

    Añadido ejercicio 10

---
+++ Tema 3/Ejercicio10.java
@@ -0,0 +1,25 @@
+/*
+ * Tema 3
+ * 
+ * Ejercicio 10
+ * 
+ * Realiza un conversor de Mb a Kb.
+ */
+package tema.pkg3;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Conversor de Mb a Kb");
+        
+        System.out.print("Introduzca los Mb: ");
+        double mb = s.nextDouble();
+        
+        System.out.printf("%1.2f %s %.2f %2s", mb, "Mb son", (mb * 1000), "Kb");
+    }
+    
+}
