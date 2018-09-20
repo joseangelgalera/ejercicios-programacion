23a9b6d1424e565356a832accdd65110080235a5
José Ángel Galera <joseangelgg96@gmail.com>
2018-09-20 09:50:49 +0200

    Añadido ejercicio 5

---
+++ Tema 4/Ejercicio05.java
@@ -0,0 +1,29 @@
+/*
+ * Tema 4
+ *
+ * Ejercicio 5
+ *
+ * Realiza un programa que resuelva una ecuación de primer grado (del tipo ax+
+ * b = 0).
+ */
+package tema.pkg4;
+import java.util.Scanner;
+
+public class Ejercicio05 {
+  public static void main(String [] args){
+    Scanner s = new Scanner(System.in);
+    
+    System.out.println("Calculadora de ecucaciones de primer grado ax + b = 0");
+    System.out.print("Introduce a: ");
+    double a = s.nextInt();
+            
+    System.out.print("Introduce b: ");
+    double b = s.nextInt();
+    
+    double x = (-b/a);
+    
+    System.out.print("x = " + x);
+  
+}
+  
+}
