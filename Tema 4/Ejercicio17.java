3fa721b6ce805b304b5fa79d4db273f2ad3668ba
José Ángel Galera <joseangelgg96@gmail.com>
2018-09-27 19:32:04 +0200

    Añadido ejercicio 17

---
+++ Tema 4/Ejercicio17.java
@@ -0,0 +1,22 @@
+/*
+ * Tema 4
+ * 
+ * Ejercicio 17
+ * 
+ * Escribe un programa que diga cuál es la última cifra de un número entero
+ * introducido por teclado.
+ */
+package tema.pkg4;
+import java.util.Scanner;
+
+public class Ejercicio17 {
+  public static void main (String[]args){
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Introduce un número: ");
+    int x = s.nextInt();
+    int ultimo = x % 10;
+    
+    System.out.print("El ultimo numero es " + ultimo + ".");
+  }
+}
