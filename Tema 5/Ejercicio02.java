547b69c4365230b20382f16b37a4cdd3045e0cdd
José Ángel Galera <joseangelgg96@gmail.com>
2018-09-30 16:26:40 +0200

    Añadido ejercicio 2

---
+++ Tema 5/Ejercicio02.java
@@ -0,0 +1,19 @@
+/*
+ * Tema 5
+ * 
+ * Ejercicio 2
+ *
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
+ */
+package tema.pkg5;
+
+
+public class Ejercicio02 {
+  public static void main (String[]args){
+    int i = 0;
+    while (i <= 99){
+      i += 5;
+      System.out.print(i + " ");
+    }
+  }
+}
