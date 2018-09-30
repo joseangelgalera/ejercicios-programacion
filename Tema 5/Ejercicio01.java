ceea72a55eb09b6b520774b8baebd46060e89354
José Ángel Galera <joseangelgg96@gmail.com>
2018-09-30 16:20:53 +0200

    Añadido tema 5 y ejercicio 1

---
+++ Tema 5/Ejercicio01.java
@@ -0,0 +1,16 @@
+/*
+ * Tema 5
+ * 
+ * Ejercicio 1
+ *
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+ */
+package tema.pkg5;
+
+public class Ejercicio01 {
+  public static void main (String[]args){
+    for ( int i = 0; i <= 100; i += 5){
+      System.out.print(i + " ");
+    }
+  }
+}
