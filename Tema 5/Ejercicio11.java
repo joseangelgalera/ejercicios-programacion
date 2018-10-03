24600db5c5159f1d585cef1dac49b4a67cb1e0c1
José Ángel Galera <joseangelgg96@gmail.com>
2018-10-03 08:37:02 +0200

    Añadido ejercicio 11

---
+++ Tema 5/Ejercicio11.java
@@ -0,0 +1,30 @@
+/*
+ * Tema 5
+ * 
+ * Ejercicio 11
+ * 
+ * Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de
+ * los 5 primeros números enteros a partir de uno que se introduce por teclado.
+ */
+package tema.pkg5;
+
+import java.util.Scanner;
+
+public class Ejercicio11 {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Introduzca un número entero : ");
+    int x = s.nextInt();
+    int i = x;
+    
+    System.out.println(" x     x²    x³");
+    System.out.println("---------------");
+    
+    while (i < x + 5) {
+      System.out.printf("%2d %5d %5d\n", i, i * i, i * i * i);
+      i++;
+    }
+  }
+}
