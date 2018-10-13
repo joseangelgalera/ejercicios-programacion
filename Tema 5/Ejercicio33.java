06a7f650fe39723ba086e2e12ba198f73baafaff
José Ángel Galera <joseangelgg96@gmail.com>
2018-10-13 14:12:03 +0200

    Añadido ejercicio 33

---
+++ Tema 5/Ejercicio33.java
@@ -0,0 +1,29 @@
+/*
+ * Tema 5
+ * 
+ * Ejercicio 33
+ *
+ * Realiza un programa que pinte la letra U por pantalla hecha con asteriscos. El
+ * programa pedirá la altura. Fíjate que el programa inserta un espacio y pinta
+ * dos asteriscos menos en la base para simular la curvatura de las esquinas
+ * inferiores.
+ * Ejemplo 1:
+ * Introduzca la altura de la U: 5
+ *  *     *
+ *  *     *
+ *  *     *
+ *  *     *
+ *   * * *
+ */
+package tema.pkg5;
+
+import java.util.Scanner;
+
+public class Ejercicio33 {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+  }
+  
+}
