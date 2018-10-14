5d9b6cd38cca59e46e23ce30ad59c05a58f4113c
José Ángel Galera <joseangelgg96@gmail.com>
2018-10-14 13:28:38 +0200

    Añadido ejercicio 43

--- Tema 5/Ejercicio43.java
+++ Tema 5/Ejercicio43.java
@@ -3,7 +3,17 @@
  *
  * Ejercicio 43
  *
- *
+ * Escribe un programa que permita partir un número introducido por teclado en
+ * dos partes. Las posiciones se cuentan de izquierda a derecha empezando por
+ * el 1. Suponemos que el usuario introduce correctamente los datos, es decir,
+ * el número introducido tiene dos dígitos como mínimo y la posición en la que
+ * se parte el número está entre 2 y la longitud del número. No se permite en
+ * este ejercicio el uso de funciones de manejo de String (por ej. para extraer
+ * subcadenas dentro de una cadena).
+ * Ejemplo:
+ * Por favor, introduzca un número entero positivo: 406783
+ * Introduzca la posición a partir de la cual quiere partir el número: 5
+ * Los números partidos son el 4067 y el 83.
  */
 package tema.pkg5;
 
