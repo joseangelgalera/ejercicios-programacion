93f104a209e8bd96a95a0371692841f9cd32b68b
José Ángel Galera <joseangelgg96@gmail.com>
2018-10-14 12:29:23 +0200

    Añadido ejercicio  41

--- Tema 5/Ejercicio41.java
+++ Tema 5/Ejercicio41.java
@@ -21,7 +21,8 @@ public class Ejercicio41 {
 
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-    System.out.println("Introduce un número para saber cuántos digitos pares e impares tiene: ");
+    System.out.println("Introduce un número para saber cuántos digitos pares e "
+            + "impares tiene: ");
     int numero = s.nextInt();
     int digito = 0;
     int pares = 0;
