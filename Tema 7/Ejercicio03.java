9c6dbcbd50ff80503936dfd3f07c4331fa06ef7b
José Ángel Galera <joseangelgg96@gmail.com>
2018-11-06 19:55:49 +0100

    Actualizado

--- Tema 7/Ejercicio03.java
+++ Tema 7/Ejercicio03.java
@@ -1,21 +1,31 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Tema 7
+ *
+ * Ejercicio 3
+ *
+ * Escribe un programa que lea 10 números por teclado y que luego los muestre
+ * en orden inverso, es decir, el primero que se introduce es el último en
+ * mostrarse y viceversa.
  */
 package tema.pkg7;
 
-/**
- *
- * @author José Ángel
- */
+import java.util.Scanner;
+
 public class Ejercicio03 {
 
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner s = new Scanner(System.in);
+
+    int[] num = new int[10];
+
+    for (int i = 0; i <= 9; i++) { // Guardamos en un array los números
+      System.out.print("Por favor, introduzca un entero: ");
+      num[i] = s.nextInt();
+    }
+
+    for (int i = 9; i >= 0; i--) { // Recorremos el array del final al principio
+      System.out.print(num[i] + " ");
+    }
   }
-  
+
 }
--- Tema 7/Ejercicio04.java
+++ Tema 7/Ejercicio04.java
@@ -8,7 +8,7 @@
  * En el
  * array cuadrado se deben almacenar los cuadrados de los valores que hay en el
  * array numero. En el array cubo se deben almacenar los cubos de los valores 
- * que hay en numero. A continuación, muestra el contenido de los tres arrays 
+ * que hay en numero. A continuación, muestra el contenido de los tres arrays
  * dispuesto en tres columnas.
  */
 package tema.pkg7;
