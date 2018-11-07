a02e19be9da90cd848ea2c517cb8d480fde66ae4
José Ángel Galera <joseangelgg96@gmail.com>
2018-11-07 08:58:13 +0100

    Añadido ejercicio 6

---
+++ Tema 7/Ejercicio06.java
@@ -0,0 +1,20 @@
+/*
+ * Tema 6
+ * 
+ * Ejercicio 6
+ *
+ * Escribe un programa que lea 15 números por teclado y que los almacene en un
+ * array. Rota los elementos de ese array, es decir, el elemento de la posición 
+ * 0 debe pasar a la posición 1, el de la 1 a la 2, etc. El número que se 
+ * encuentra en la última posición debe pasar a la posición 0. Finalmente, 
+ * muestra el contenido del array.
+ */
+package tema.pkg7;
+
+public class Ejercicio06 {
+
+  public static void main(String[] args) {
+    
+  }
+  
+}
